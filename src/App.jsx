614735abb1f7517a9b325498ef8fd8f3cc9afeca
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-01-09 14:53:12 -0800

    setting up webpack, package.json, and starting client files for product

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, './index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    host: 'localhost',
+    hot: true,
+    open: true,
+    port: 8080,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+};
