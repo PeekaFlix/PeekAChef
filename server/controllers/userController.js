53abf739a4e40a7abf4bdcacc9977e8674d3eb25
Tony Lim <tonylim9438@gmail.com>
2023-01-10 11:43:02 -0800

    second commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,23 @@
+const db = 'require(userModel)...placeholder'
 const usersController = {};
 
-usersController.createUser = (req, res, next) => {
-
-}
+usersController.createUser = (req, res, next, err) => {
+    const createQuery = `INSERT INTO users (
+        username
+        password
+    ) VALUES VALUES ($1)`;
+    const {
+        username,
+        password
+    } = req.body;
+    db.query(createQuery, ([username, password])
+    .then((data) => {
+        return next();
+    })
+    .catch((err) => {
+        console.log(`an err has occured: ${err}`)
+    })
+)};
 
 usersController.checkLogin = (req, res, next) => {
 
