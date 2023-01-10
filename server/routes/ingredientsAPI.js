e25d162208aec0db451f7d8983e2fe31f77febcc
Sylvia <110571069+sylvia45335@users.noreply.github.com>
2023-01-10 14:55:46 -0800

    fixed server errors, erased middleware placeholders, and exported routers

--- server/routes/ingredientsAPI.js
+++ server/routes/ingredientsAPI.js
@@ -4,21 +4,23 @@ const router = express.Router();
 const middleware = 'placeholder';
 
 /* retrieves ingredients */
-router.get('/getIngredients', middleware, (req, res) => {
+router.get('/getIngredients', (req, res) => {
     return res.status(200)
 })
 
 /* creates/adds new ingredients */
-router.post('/addIngredients', middleware, (req, res) => {
+router.post('/addIngredients', (req, res) => {
     return res.status(200)
 })
 
 /* updates ingredients */
-router.put('/updateIngredients', middleware, (req, res) => {
+router.put('/updateIngredients', (req, res) => {
     return res.status(200)
 })
 
 /* delete ingredient */
-router.delete('/deleteRecipe', middleware, (req, res) => {
+router.delete('/deleteRecipe', (req, res) => {
     return res.status(200)
 })
+
+module.exports = router;
